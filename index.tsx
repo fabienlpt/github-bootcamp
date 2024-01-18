30f206c5698708debf638f20eb287cd4d4b04c48
Fabien Lapert <lapertfabien@gmail.com>
2024-01-18 11:49:09 +0100

    feature(theme): update theme

--- index.tsx
+++ index.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Deck, DefaultTemplate, MarkdownSlideSet } from 'spectacle';
+import { Deck, DefaultTemplate, MarkdownSlideSet, defaultTheme } from 'spectacle';
 import mdContent from './slides.md';
 
-const Presentation = () => (
-  <Deck template={() => <DefaultTemplate />}>
+
+const Presentation = () => {
+const customTheme = {
+  ...defaultTheme,
+  colors: {
+    primary: '#f6f7f0',
+    secondary: '#f6f7f0',
+    tertiary: '#333333',
+  },
+  fonts: {
+    header: 'Helvetica, sans-serif',
+    text: 'Arial, sans-serif',
+    monospace: 'Consolas, "Courier New", monospace'
+  }
+};
+
+  return (
+  <Deck template={() => <DefaultTemplate />} theme={customTheme}>
     <MarkdownSlideSet>{mdContent}</MarkdownSlideSet>
   </Deck>
-);
+)};
 
 createRoot(document.getElementById('app')!).render(<Presentation />);
